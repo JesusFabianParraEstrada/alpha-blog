697fd0613d9880c8a3da2311bed758302cadfa7f
JesusFabianParraEstrada <fabian.parra@skydropx.com>
2022-04-21 22:45:00 -0700

    Create resource articles

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'home', to: "pages#home"
   get 'about', to: "pages#about"
+  #resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  resources :articles
 end
